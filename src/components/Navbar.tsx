dbb898989fee73be72c1ec2a45234ffa17bb6fdc
Novo1999 <neptune.navo@gmail.com>
2024-02-17 18:36:31 +0600

    navbar is now client only

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,25 +3,28 @@ import Dropdown from './Dropdown'
 import TopMenu from './TopMenu'
 import useInitAuth from '@/hooks/use-init-auth'
 import { Player } from '@lottiefiles/react-lottie-player'
+import ClientOnly from './PostJob/ClientOnly'
 const Navbar = () => {
   return (
-    <nav className='mt-4 flex justify-between px-4 items-center'>
-      <Link href='/' className='text-2xl flex font-chewy items-center'>
-        <Player
-          className='h-12 w-12 top-0 right-0'
-          autoplay
-          loop
-          src='https://lottie.host/2d6ee42c-040e-404a-aa53-25ea53fc95d6/BqgS1kD7XT.json'
-        />
-        RemoteJumbo
-      </Link>
-      <div className='block sm:hidden'>
-        <Dropdown />
-      </div>
-      <div className='hidden sm:block'>
-        <TopMenu />
-      </div>
-    </nav>
+    <ClientOnly>
+      <nav className='mt-4 flex justify-between px-4 items-center'>
+        <Link href='/' className='text-2xl flex font-chewy items-center'>
+          <Player
+            className='h-12 w-12 top-0 right-0'
+            autoplay
+            loop
+            src='https://lottie.host/2d6ee42c-040e-404a-aa53-25ea53fc95d6/BqgS1kD7XT.json'
+          />
+          RemoteJumbo
+        </Link>
+        <div className='block sm:hidden'>
+          <Dropdown />
+        </div>
+        <div className='hidden sm:block'>
+          <TopMenu />
+        </div>
+      </nav>
+    </ClientOnly>
   )
 }
 export default Navbar
